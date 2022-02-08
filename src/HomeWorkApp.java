eb89258d2474d2dface7f73175e9c490c9ed23d7
pitmak <piter_makarov@mail.ru>
2022-02-08 19:20:58 +0500

    Первая домашка

---
+++ src/HomeWorkApp.java
@@ -0,0 +1,43 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+    }
+
+    public static void checkSumSign() {
+        int a = 1;
+        int b = 2;
+
+        if (a+b >= 0)
+            System.out.println("Сумма положительная");
+        else
+            System.out.println("Сумма отрицательная");
+    }
+
+    public static void printColor() {
+        int value = 3;
+
+        if (value <= 0)
+            System.out.println("Красный");
+        else if (value <= 100)
+            System.out.println("Желтый");
+        else
+            System.out.println("Зеленый");
+    }
+
+    public static void compareNumbers() {
+        int a = 4;
+        int b = 5;
+
+        if (a >= b)
+            System.out.println("a >= b");
+        else
+            System.out.println("a < b");
+    }
+}
